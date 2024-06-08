1592555cadc6c48b22e2663558de0e0da8c1ceac
EdoardoPanzeri1 <edoardopanzeriwork@gmail.com>
2024-06-08 20:38:18 +0200

    add middleware and tracking number of request

---
+++ reset.go
@@ -0,0 +1,10 @@
+package main
+
+import "net/http"
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Reset successful"))
+}
